4ee7e74f5766c0cf1f364f58d6ab1af4c1550147
AlexanderYakovenko <AlexanderYakovenko@Hellens-Mac-mini.local>
2017-07-19 17:20:43 +0300

    all worked

--- CombInstagram/ViewController.swift
+++ CombInstagram/ViewController.swift
@@ -7,19 +7,48 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var shownCity = [String]()
+    let allCities = ["New York","London","Oslo","Warsaw","Berlin","Praga"]
+    
+    let disposableBag = DisposeBag()
+    
+    
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    @IBOutlet weak var tableView: UITableView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        searchBar.rx.text.orEmpty.subscribe(onNext: {[unowned self] query in
+            self.shownCity = self.allCities.filter { $0.hasPrefix(query) }
+            self.tableView.reloadData()
+        
+        }).addDisposableTo(disposableBag)
+        
+        
+        
+       
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return shownCity.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = shownCity[indexPath.row]
+        return cell
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
 }
 
